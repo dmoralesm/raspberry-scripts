b23fe89b9c6e7b9291e97a92a9fb9438411f8e7f
David Morales <git@damorales.com>
2017-10-01 00:42:53 -0500

    Get current transmission speed status

---
+++ transmission_speed.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd $( dirname $0 )
+
+. pushover_config.sh
+
+TR_SUMMARY=$( transmission-remote -n $TR_USER:$TR_PASSWORD -l | grep '^Sum' )
+UP_SPEED=$( awk '{print $4}' <<< $TR_SUMMARY )
+DOWN_SPEED=$( awk '{print $5}' <<< $TR_SUMMARY )
+
+echo "Upload speed: $UP_SPEED kB/s"
+echo "Download speed: $DOWN_SPEED kB/s"
